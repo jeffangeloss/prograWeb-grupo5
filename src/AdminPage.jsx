b64d66c10692dcb7ecbb3340c68724b2f8351934
Rafael Lau <64998960+rafaellau17@users.noreply.github.com>
2026-01-25 17:54:39 -0500

    Update AdminPage.jsx

--- src/AdminPage.jsx
+++ src/AdminPage.jsx
@@ -1,7 +1,7 @@
 function AdminPage() {
     return <div>
 
-        <div className="bg-gradient-to-r from-blue-700 to-blue-900 px-6 py-4 shadow-xl flex sm:justify-between ">
+        <div className="bg-linear-to-r from-blue-700 to-blue-900 px-6 py-4 shadow-xl flex sm:justify-between ">
             <img src="https://www.ulima.edu.pe/themes/custom/ulima/logo.svg" className="h-10" />
             <button type="button"
                 className="border shadow-xl rounded-xl px-4 py-2 bg-blue-900 hover:bg-blue-300 text-white">Usuario</button>
@@ -18,7 +18,7 @@ function AdminPage() {
                     <button type="button" className="shadow-md bg-blue-700 rounded-2xl w-64 py-2 text-white">Añadir
                         Usuario</button>
                     <button type="button"
-                        className="shadow-md bg-blue-700 rounded-2xl w-64 py-2 text-white">Exportar</button>
+                        className="shadow-md bg-blue-700 rounded-2xl w-64 py-2 text-white">Eliminar usuario</button>
                 </div>
                 <div className="ml-4 mt-6 gap-6 flex flex-auto">
                     <input className="bg-white px-10 py-1 rounded-xl shadow-xl" type="text" placeholder="Buscar usuario..."></input>
@@ -40,6 +40,7 @@ function AdminPage() {
                                 <th className="font-medium px-4 py-3">EMAIL</th>
                                 <th className="font-medium px-4 py-3">ROL</th>
                                 <th className="font-medium px-4 py-3">ÚLTIMO ACCESO</th>
+                                <th className="font-medium px-4 py-3"></th>
                             </tr>
                         </thead>
                         <tbody className="divide-y divide-blue-900/50">
@@ -53,6 +54,9 @@ function AdminPage() {
                                     <span className="px-3 py-2 rounded-2xl text-blue-300 border border-blue-500">Usuario</span>
                                 </td>
                                 <td className="px-4 py-4">25/01/2025</td>
+                                <td className="px-6 py-4 text-right">
+                                    <button className="px-3 py-1.5 text-xs rounded-md bg-orange-300/30 text-orange-300 border border-orange-500/30 hover:bg-orange-500/30 transition">Editar</button>
+                                </td>
                             </tr>
                             <tr className="hover:bg-blue-600 transition">
                                 <td className="px-4 py-4">
@@ -64,6 +68,9 @@ function AdminPage() {
                                     <span className="px-3 py-2 rounded-2xl text-red-300 border border-red-500">Administrador</span>
                                 </td>
                                 <td className="px-4 py-4">21/01/2025</td>
+                                <td className="px-6 py-4 text-right">
+                                    <button className="px-3 py-1.5 text-xs rounded-md bg-orange-300/30 text-orange-300 border border-orange-500/30 hover:bg-orange-500/30 transition">Editar</button>
+                                </td>
                             </tr>
                         </tbody>
                     </table>
