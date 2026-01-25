788c115ef2aea34ff3eab099f01428e801db3dd6
Rafael Lau <64998960+rafaellau17@users.noreply.github.com>
2026-01-25 17:35:29 -0500

    Update AdminPage.jsx

--- src/AdminPage.jsx
+++ src/AdminPage.jsx
@@ -1,10 +1,10 @@
 function AdminPage() {
     return <div>
 
-        <div className="bg-gradient-to-r from-blue-700 to-blue-900 p-4 shadow-md flex sm:justify-between ">
+        <div className="bg-gradient-to-r from-blue-700 to-blue-900 px-6 py-4 shadow-xl flex sm:justify-between ">
             <img src="https://www.ulima.edu.pe/themes/custom/ulima/logo.svg" className="h-10" />
             <button type="button"
-                className="border border-gray-500 rounded-xl p-2 bg-blue-900 hover:bg-blue-300 text-white">Usuario</button>
+                className="border shadow-xl rounded-xl px-4 py-2 bg-blue-900 hover:bg-blue-300 text-white">Usuario</button>
         </div>
         <div className="grid grid-cols-[220px_1fr] min-h-screen">
             <div className="px-3 py-2 flex flex-col bg-blue-950 gap-3">
@@ -12,56 +12,62 @@ function AdminPage() {
                 <button type="button" className="mt-4 shadow-md bg-blue-700 rounded-xl w-48 py-2 text-white">Usuarios</button>
                 <button type="button" className="shadow-md bg-blue-700 rounded-xl w-48 py-2 text-white">Dashboard</button>
             </div>
-            <div className="bg-sky-700">
-                <h1 className="mt-6 ml-6 object-left text-3xl text-white">Usuarios</h1>
-                <div className="flex gap-4 mt-4 place-self-end">
+            <div className="bg-sky-900 px-4">
+                <h1 className="mt-6 ml-6 object-left text-3xl text-white font-semibold">Usuarios</h1>
+                <div className="ml-4 flex gap-4 mt-4">
                     <button type="button" className="shadow-md bg-blue-700 rounded-2xl w-64 py-2 text-white">Añadir
                         Usuario</button>
                     <button type="button"
                         className="shadow-md bg-blue-700 rounded-2xl w-64 py-2 text-white">Exportar</button>
                 </div>
-                <div className="mt-6 gap-6 flex flex-auto px-12">
-                    <input className="bg-white px-4 py-1 rounded-xl shadow-md" type="text" placeholder="Buscar usuario..."></input>
-                    <select className="bg-white pl-4 rounded-xl shadow-md" value="Rol">
+                <div className="ml-4 mt-6 gap-6 flex flex-auto">
+                    <input className="bg-white px-10 py-1 rounded-xl shadow-xl" type="text" placeholder="Buscar usuario..."></input>
+                    <select className="bg-white pl-3 rounded-xl shadow-xl" value="Rol">
                         <option>Rol</option>
                         <option>Usuario</option>
                         <option>Administrador</option>
                     </select>
                 </div>
-                <div className="border-slate-800 mt-10 rounded-2xl shadow-md place-self-center">
-                    <table className="border text-xs text-white">
+
+                <div className="border border-black mt-10 mx-8 rounded-2xl shadow-xl overflow-hidden ">
+                    <table className="text-xs text-white text-left min-w-full">
                         <thead className="text-sm text-gray-200 bg-blue-900">
                             <tr>
-                                <th className="px-4 py-3">
+                                <th className="px-4 py-4">
                                     <input type="checkbox" className="w-4 h-4" />
                                 </th>
-                                <th className="px-4 py-3">Nombre</th>
-                                <th className="px-4 py-3">Email</th>
-                                <th className="px-4 py-3">Rol</th>
-                                <th className="px-4 py-3">Último acceso</th>
+                                <th className="font-medium px-4 py-3">NOMBRE</th>
+                                <th className="font-medium px-4 py-3">EMAIL</th>
+                                <th className="font-medium px-4 py-3">ROL</th>
+                                <th className="font-medium px-4 py-3">ÚLTIMO ACCESO</th>
                             </tr>
                         </thead>
-                        <tbody className="divide-y-2">
-                            <tr>
-                                <td className="px-4 py-3">
+                        <tbody className="divide-y divide-black">
+                            <tr className="hover:bg-blue-600 transition">
+                                <td className="px-4 py-4">
                                     <input type="checkbox" className="w-4 h-4" />
                                 </td>
-                                <td className="px-4 py-3">Isabella Stanley</td>
-                                <td className="px-4 py-3">stanley.li@hotmail.com</td>
-                                <td className="px-4 py-3">Usuario</td>
-                                <td className="px-4 py-3">25/01/2025</td>
+                                <td className="px-4 py-4">Isabella Stanley</td>
+                                <td className="px-4 py-4">stanley.li@hotmail.com</td>
+                                <td>
+                                    <span className="px-3 py-2 rounded-2xl text-blue-300 border border-blue-500">Usuario</span>
+                                </td>
+                                <td className="px-4 py-4">25/01/2025</td>
                             </tr>
-                            <tr>
-                                <td className="px-4 py-3">
+                            <tr className="hover:bg-blue-600 transition">
+                                <td className="px-4 py-4">
                                     <input type="checkbox" className="w-4 h-4" />
                                 </td>
-                                <td className="px-4 py-3">Jose Blake</td>
-                                <td className="px-4 py-3">jose.blake@gmail.com</td>
-                                <td className="px-4 py-3">Administrador</td>
-                                <td className="px-4 py-3">21/01/2025</td>
+                                <td className="px-4 py-4">Jose Blake</td>
+                                <td className="px-4 py-4">jose.blake@gmail.com</td>
+                                <td>
+                                    <span className="px-3 py-2 rounded-2xl text-red-300 border border-red-500">Administrador</span>
+                                </td>
+                                <td className="px-4 py-4">21/01/2025</td>
                             </tr>
                         </tbody>
                     </table>
+                    <span class="p-2 text-white text-xs">Usuarios: 2</span>
                 </div>
             </div>
         </div>
