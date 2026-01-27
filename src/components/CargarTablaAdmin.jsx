d4445ff0e92e8a597e8231f3bdd06bc38c7da925
Rafael Lau <64998960+rafaellau17@users.noreply.github.com>
2026-01-27 15:18:28 -0500

    Componerizacion de carga TablaAdmin, Diseño NavBarAdmin y SeguridadUsuarioPage.jsx

--- src/components/CargarTablaAdmin.jsx
+++ src/components/CargarTablaAdmin.jsx
@@ -16,6 +16,10 @@ function CargarTablaAdmin({ listaUsuarios }) {
                         </td>
                         <td className="px-4 py-4">{usuario.ultimoAcceso}</td>
                         <td className="px-6 py-4 text-right">
+                            <button className="px-3 py-1.5 text-xs rounded-xl bg-emerald-500/10 text-emerald-400 border border-emerald-500/30 hover:bg-emerald-500/20 transition"
+                                onClick={function () { navigate("/seguridadUsuario") }}>Seguridad</button>
+                        </td>
+                        <td className="px-6 py-4 text-right">
                             <button className="px-3 py-1.5 text-xs rounded-xl bg-orange-300/30 text-orange-300 border border-orange-500/30 hover:bg-orange-500/30 transition"
                                 onClick={function () { navigate("/editarUsuario") }}>Editar</button>
                         </td>
