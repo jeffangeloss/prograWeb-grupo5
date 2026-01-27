eeac0c5a31990529e4ebf2148bb29fdba3193af0
Rafael Lau <64998960+rafaellau17@users.noreply.github.com>
2026-01-27 11:28:31 -0500

    Arreglo ReactRouter

--- src/components/CrearUsuarioAdminForm.jsx
+++ src/components/CrearUsuarioAdminForm.jsx
@@ -8,7 +8,7 @@ function CrearUsuarioForm() {
 
             <div className="mb-6">
                 <label className="text-white mb-2 ml-1">Nombre completo</label>
-                <input type="text" placeholder="Nombre del usuario" className="w-full mt-2 px-4 py-2 rounded-xl shadow-md bg-white text-black" />
+                <input type="text" className="w-full mt-2 px-4 py-2 rounded-xl shadow-md bg-white text-black" />
             </div>
 
             <div className="mb-6">
@@ -37,11 +37,11 @@ function CrearUsuarioForm() {
             <div className="flex justify-end gap-4 pt-4">
                 <button
                     className="px-4 py-2 rounded-xl bg-gray-300 text-gray-700 border border-gray-300 hover:bg-gray-50/30 transition"
-                    onClick={() => navigate("/admin")}>Cancelar</button>
+                    onClick={function() {navigate("/admin")}}>Cancelar</button>
 
                 <button
-                    className="px-4 py-2 rounded-xl bg-blue-600 text-white border border-blue-900 hover:bg-blue-700/30 shadow-md transition"
-                    onClick={() => navigate("/admin")}>Crear usuario</button>
+                    className="px-4 py-2 rounded-xl bg-blue-600 text-white border border-blue-900 hover:bg-blue-700/30 transition"
+                    onClick={function() {navigate("/admin")}}>Crear usuario</button>
             </div>
         </div>
     </div>
