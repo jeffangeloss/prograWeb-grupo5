a438ea60ceae6cd3dd45c6c700aac4968550a2d0
Rafael Lau <64998960+rafaellau17@users.noreply.github.com>
2026-01-27 19:54:34 -0500

    useState en AdminForm

--- src/components/CrearUsuarioAdminForm.jsx
+++ src/components/CrearUsuarioAdminForm.jsx
@@ -1,34 +1,54 @@
 import { useNavigate } from "react-router-dom"
 
-function CrearUsuarioForm() {
+function CrearUsuarioForm({ crearUsuarioAdmin }) {
     const navigate = useNavigate()
+
+    const [nombre, setNombre] = useState("")
+    const [email, setEmail] = useState("")
+    const [contra, setContra] = useState("")
+    const [confirmarContra, setConfirmarContra] = useState("")
+    const [rol, setRol] = useState("Usuario")
+
+    function contraseñasIguales() {
+        if (contra == confirmarContra) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
 
     return <div>
         <div className="bg-gray-800/50 rounded-2xl shadow-xl p-8 min-w-xl lg:min-w-2xl place-self-center">
 
             <div className="mb-6">
                 <label className="text-white mb-2 ml-1">Nombre completo</label>
-                <input type="text" className="w-full mt-2 px-4 py-2 rounded-xl shadow-md bg-white text-black" />
+                <input type="text" className="w-full mt-2 px-4 py-2 rounded-xl shadow-md bg-white text-black"
+                    value={nombre} onChange={function (ev) { setNombre(ev.target.value) }} />
             </div>
 
             <div className="mb-6">
                 <label className="text-white mb-2 ml-1">Correo electrónico</label>
-                <input type="email" placeholder="correo@ejemplo.com" className="w-full mt-2 px-4 py-2 rounded-xl shadow-md bg-white text-black" />
+                <input type="email" placeholder="correo@ejemplo.com" className="w-full mt-2 px-4 py-2 rounded-xl shadow-md bg-white text-black"
+                    value={email} onChange={function (ev) { setEmail(ev.target.value) }} />
             </div>
 
             <div className="mb-6">
                 <label className="text-white mb-2 ml-1">Contraseña</label>
-                <input type="password" className="w-full mt-2 px-4 py-2 rounded-xl shadow-md bg-white text-black" />
+                <input type="password" className="w-full mt-2 px-4 py-2 rounded-xl shadow-md bg-white text-black"
+                    value={contra} onChange={function (ev) { setContra(ev.target.value) }} />
             </div>
 
             <div className="mb-6">
                 <label className="text-white mb-2 ml-1">Confirmar Contraseña</label>
-                <input type="password" className="w-full mt-2 px-4 py-2 rounded-xl shadow-md bg-white text-black" />
+                <input type="password" className="w-full mt-2 px-4 py-2 rounded-xl shadow-md bg-white text-black"
+                    value={confirmarContra} onChange={function (ev) { setConfirmarContra(ev.target.value) }} />
             </div>
 
             <div className="mb-8">
                 <label className="text-white mb-2 ml-1">Rol</label>
-                <select className="w-full mt-2 px-4 py-2 rounded-xl shadow-md bg-white text-black">
+                <select className="w-full mt-2 px-4 py-2 rounded-xl shadow-md bg-white text-black"
+                    value={rol} onChange={function (ev) { setRol(ev.target.value) }}>
                     <option>Usuario</option>
                     <option>Administrador</option>
                 </select>
@@ -37,11 +57,11 @@ function CrearUsuarioForm() {
             <div className="flex justify-end gap-4 pt-4">
                 <button
                     className="px-4 py-2 rounded-xl bg-gray-300 text-gray-700 border border-gray-300 hover:bg-gray-50/30 transition"
-                    onClick={function() {navigate("/admin")}}>Cancelar</button>
+                    onClick={function () { navigate("/admin") }}>Cancelar</button>
 
                 <button
                     className="px-4 py-2 rounded-xl bg-blue-600 text-white border border-blue-900 hover:bg-blue-700/30 transition"
-                    onClick={function() {navigate("/admin")}}>Crear usuario</button>
+                    onClick={function () { crearUsuarioAdmin(), navigate("/admin") }}>Crear usuario</button>
             </div>
         </div>
     </div>
--- src/pages/CrearUsuarioAdminPage.jsx
+++ src/pages/CrearUsuarioAdminPage.jsx
@@ -6,7 +6,7 @@ function CrearUsuarioAdminPage() {
     <div className="bg-[url('/img/azul.png')] bg-cover min-h-screen bg-center">
       <NavBarAdmin />
       <div className="px-6 py-6">
-        <h1 className="pb-5 ml-6 object-left text-3xl text-white font-semibold">Añadir Usuario</h1>
+        <h1 className="pb-5 ml-6 text-center text-3xl text-white font-semibold">Añadir Usuario</h1>
         <CrearUsuarioAdminForm />
       </div>
     </div>
