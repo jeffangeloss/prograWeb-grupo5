bc4f876e37a78f31594d645474a041e4fcb64e66
gabylolicious <gabylolicious@gmail.com>
2026-01-28 20:19:28 -0500

    intento solucion de landscape mobile

--- src/components/auth/Azul.jsx
+++ src/components/auth/Azul.jsx
@@ -1,7 +1,7 @@
 function Azul() {
     return (
         <section
-            className="h-20 md:h-screen w-full"
+            className="h-20 md:h-[100svh] w-full"
             style={{
                 backgroundImage: "url('/img/azul.png')",
                 backgroundSize: "cover",
